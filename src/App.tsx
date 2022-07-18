faa266005b797bd0db281ae992651e895350c88e
Bernard <bero4net@gmail.com>
2022-07-18 09:34:43 +0200

    feat: mina-signer-wasm keygen example

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import init, {Client} from "@chainsafe/mina-signer-wasm";
+import {Keypair} from "@chainsafe/mina-signer-wasm/node";
 
 function App() {
+  const [state, setState] = React.useState<string | Keypair>("Getting Keypair");
+  React.useEffect(() => {
+    (async () => {
+      await init();
+      generateKey();
+    })();
+  }, []);
+
+  const generateKey = () => {
+    const client = new Client({ network: "testnet" });
+    setState(client.genKeys());
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {typeof state === "string" ? state :
+          <React.Fragment>
+            <code>Public Key: {state.publicKey}</code>
+            <code>Private Key: {state.privateKey}</code>
+          </React.Fragment>
+        }
+        <br />
+        <a className="App-link" onClick={generateKey}>Get a new key</a>
       </header>
     </div>
   );
